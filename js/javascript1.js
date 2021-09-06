5ade83eb8bab7009ac5b30c1359e5cff9b33c674
Remigio Hernández López <remigioh@hotmail.com>
2021-09-05 20:15:51 -0500

    Add files via upload

---
+++ js/javascript1.js
@@ -0,0 +1,20 @@
+ const app = Vue.createApp({
+    data() {
+        return {
+            title:'Areás de saberes',
+            image:'./img/saberes1.jpg',
+            description: 'Categorias de recursos digitales disponibles'
+        }
+    }
+});
+
+/*
+
+let app = new Vue({
+  el: '#app',
+  data: {
+    title:'Areás de saberes',
+    image:'./img/saberes1.jpg',
+    description: 'Categorias de recursos digitales disponibles'
+  }
+});
